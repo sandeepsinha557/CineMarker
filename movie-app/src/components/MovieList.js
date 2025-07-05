782a3f65088bd4364fc30ef3179a018eee446184
Sandeep Sinha <sandeepsinha112233@gmail.com>
2025-07-05 20:32:19 +0530

    first commit

--- movie-app/src/components/MovieList.js
+++ movie-app/src/components/MovieList.js
@@ -1,34 +1,43 @@
-import React from 'react';
+import React from "react";
 
-const MovieList = ({ movies, handleFavouritesClick, handleMovieClick, favouriteComponent }) => {
-	const FavouriteComponent = favouriteComponent;
+const MovieList = ({
+  movies,
+  handleFavouritesClick,
+  handleMovieClick,
+  favouriteComponent,
+}) => {
+  const FavouriteComponent = favouriteComponent;
 
-	return (
-		<>
-			{movies.map((movie, index) => (
-				<div
-					className='image-container d-flex justify-content-start m-3'
-					key={index}
-					onClick={() => handleMovieClick(movie)}
-				>
-					<img
-                    src={movie.Poster!=="N/A"?movie.Poster:"https://via.placeholder.com/300x445?text=No+Image"}
-                    alt={movie.Title}
-                    />
+  return (
+    <>
+      {movies.map((movie, index) => (
+        <div
+          className="image-container d-flex justify-content-start m-3"
+          key={index}
+          onClick={() => handleMovieClick(movie)}
+        >
+          <img
+            src={
+              movie.Poster !== "N/A"
+                ? movie.Poster
+                : "https://via.placeholder.com/300x445?text=No+Image"
+            }
+            alt={movie.Title}
+          />
 
-					<div
-						onClick={(e) => {
-							e.stopPropagation(); // Prevent modal opening
-							handleFavouritesClick(movie);
-						}}
-						className='overlay d-flex align-items-center justify-content-center'
-					>
-						<FavouriteComponent />
-					</div>
-				</div>
-			))}
-		</>
-	);
+          <div
+            onClick={(e) => {
+              e.stopPropagation(); // Prevent modal opening
+              handleFavouritesClick(movie);
+            }}
+            className="overlay d-flex align-items-center justify-content-center"
+          >
+            <FavouriteComponent />
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default MovieList;
